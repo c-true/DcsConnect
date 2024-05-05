1b9b02ac6063ae0b6f47aaca3c5299886657f130
Timian Heber <timian.heber@c-true.com>
2024-05-05 13:27:44 +0200

    Cleaned up DCS Console a little with SpectreConsole

--- src/CTrue.DcsConnect.Console/Program.cs
+++ src/CTrue.DcsConnect.Console/Program.cs
@@ -1,38 +1,75 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.ComponentModel;
 using System.Security.Cryptography.X509Certificates;
 using CTrue.DcsConnect;
 using RurouniJones.Dcs.Grpc.V0.Mission;
+using Spectre.Console;
+using Spectre.Console.Cli;
 
 Console.WriteLine("C-True DCS Console");
 
 if (args.Length == 0)
 {
     Console.WriteLine("Usage: DcsConsole <server_name>");
-    return;
+    return 0;
 }
 
-DcsConnector dcsConnect = new DcsConnector(new MyLogProvider());
-dcsConnect.ConnectionStatusChanged += (sender, eventArgs) => Console.WriteLine("Connected");
+var app = new CommandApp<ConnectCommand>();
+return app.Run(args);
 
-Action<DcsUnitUpdate> myUnitHandler = update =>
+
+public class ConnectCommand : Command<ConnectCommand.ConnectSettings>
 {
-    Console.WriteLine("Received update: " +  update.Unit.Name);
-};
+    public class ConnectSettings : CommandSettings
+    {
+        [CommandOption("-s|--server")]
+        public string? Server { get; set; }
 
-Action<StreamEventsResponse> eventHandler = response => { Console.WriteLine("Event"); };
+        [CommandOption("-p|--port")]
+        [DefaultValue(50051)]
+        public ushort Port { get; set; }
+    }
 
-dcsConnect.SubscribeUnitUpdates(myUnitHandler);
-dcsConnect.SubscribeEventUpdates(eventHandler);
+    public override int Execute(CommandContext context, ConnectSettings settings)
+    {
+        try
+        {
+            DcsConnector dcsConnect = new DcsConnector(new MyLogProvider());
+            dcsConnect.ConnectionStatusChanged += (sender, eventArgs) => AnsiConsole.MarkupLine("[green]Connected.[/]");
 
-string serverAddr = args[0];
+            Action<DcsUnitUpdate> myUnitHandler = update =>
+            {
+                AnsiConsole.MarkupLine($"Received update: [green]{update.Unit.Name}[/]");
+            };
 
-dcsConnect.Connect(serverAddr, 50051, "DcsConsole");
+            Action<StreamEventsResponse> eventHandler = response => { Console.WriteLine($"Event: {response.EventCase}"); };
 
-Console.ReadKey();
-dcsConnect.Units.ForEach(x => Console.WriteLine(x.Callsign));
-Console.ReadKey();
+            dcsConnect.SubscribeUnitUpdates(myUnitHandler);
+            dcsConnect.SubscribeEventUpdates(eventHandler);
 
+            string serverAddr = settings.Server ?? "";
+
+            AnsiConsole.MarkupLine($"Connecting to [green]{settings.Server}:{settings.Port}[/]");
+
+            dcsConnect.Connect(serverAddr, settings.Port, "DcsConsole");
+
+            Console.WriteLine("Press any key to list current units");
+            Console.ReadKey();
+            dcsConnect.Units.ForEach(x => Console.WriteLine(x.Callsign));
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.MarkupLine($"[red]ERROR: {e.Message}[/]");
+        }
+
+        // Omitted
+        return 0;
+    }
+}
 
 public class MyLogProvider : IDcsConnectLogProvider
 {
